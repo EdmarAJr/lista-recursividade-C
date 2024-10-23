a5e5bf9dc4fb8971276791e4237b277bf6bd53f0
Edmar Amorim <trovador@hotmail.com.br>
2024-10-23 10:34:17 -0300

    Create: fibonacci.c
    Faca uma funcao recursiva que calcule e retorne o N-esimo termo da sequencia Fibonacci. Alguns numeros desta sequência sao: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89...

---
+++ fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+/*2) Faca uma funcao recursiva que calcule e retorne o N-esimo termo da sequencia Fibonacci. Alguns numeros desta sequencia sao: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89...*/ 
+
+/*0 1 1 2 3 5 8 13*/
+int fibonacci(int n) {
+    if (n == 1) return 0;
+    if (n == 2) return 1;
+
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+int main() {
+    int n =6;
+
+    printf("Sequência de Fibonacci ateh %d termos eh: %d\n", n, fibonacci(n));
+
+    return 0;
+}
