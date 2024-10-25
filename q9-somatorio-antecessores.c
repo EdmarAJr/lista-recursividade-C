83ff31bd25e31d93b5bdcf15b75a72c6abd51fe3
Edmar Amorim <trovador@hotmail.com.br>
2024-10-25 10:06:58 -0300

    Create q9-somatorio-antecessores.c
    Crie uma funcao recursiva que receba um numero inteiro positivo N e calcule o somatorio dos numeros de 1 a N.

---
+++ q9-somatorio-antecessores.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*9) Crie uma funcao recursiva que receba um numero inteiro positivo N e calcule o somatorio dos numeros de 1 a N.*/ 
+
+int somarNum (int n){
+     if (n < 0) return -1;
+     if (n == 0) return 0;
+
+    // Soma o ultimo elemento do vetor com a soma dos anteriores
+    return n + somarNum(n-1);
+}
+
+int main(){
+    int num = 5;//15
+    printf("Somatorio de 0 ateh %d eh: %d", num, somarNum(num));
+    return 0;
+}
