f10dccccd91e661be0b0fac84356dc8e1df55f88
Edmar Amorim <trovador@hotmail.com.br>
2024-10-25 10:14:07 -0300

    Update and rename: fibonacci.c to q02-fibonacci.c
    Faca uma funcao recursiva que calcule e retorne o N-esimo termo da sequencia Fibonacci. Alguns numeros desta sequencia sao: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89...

--- q02-fibonacci.c
+++ q02-fibonacci.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <string.h>
 
 /*2) Faca uma funcao recursiva que calcule e retorne o N-esimo termo da sequencia Fibonacci. Alguns numeros desta sequencia sao: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89...*/ 
 
