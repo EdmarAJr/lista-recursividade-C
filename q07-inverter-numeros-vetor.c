d3720616d0581d9bca7abdf4dfd4fdfff6fa1f6f
Edmar Amorim <trovador@hotmail.com.br>
2024-10-31 09:13:23 -0300

    Update: q07-inverter-numeros-vetor.c
    Crie um programa em C que receba um vetor de numeros reais com 100 elementos. Escreva uma funcao recursiva que inverta ordem dos elementos presentes no vetor.

--- q07-inverter-numeros-vetor.c
+++ q07-inverter-numeros-vetor.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 /*7) Crie um programa em C que receba um vetor de numeros reais com 100 elementos. Escreva uma funcao recursiva que inverta ordem dos elementos presentes no vetor.*/  
-int inverter(int v[], int tam){
+
+int inverter(int v[], int tam){ //altera o vetor original
     if (tam <= 1) return 0;
 
     // Troca o primeiro e o ultimo elemento
@@ -15,47 +16,35 @@ int inverter(int v[], int tam){
     return inverter(v + 1, tam - 2);
 }
 
-int main(){
-    int vetor[10] = {0,1,2,3,4,5,6,7,8,9};
-    
-    printf("Vetor antes da inversao: ");
-    for(int i=0; i<10;i++){
-        printf("%d ", vetor[i]);
-    }
-    printf("\nVetor apos a inversao: ");
-    inverter(vetor, 10);
-    for(int i=0; i<10;i++){
-        printf("%d ", vetor[i]);
-    }
-    return 0;
-}
-
-//ou
-
-void inverter(int v[], int i, int tam){
+void inverter2(int v[], int i, int tam){
     if (i>=tam/2) return;
 
-    // Troca o primeiro e o ultimo elemento
     int temp = v[i];
     v[i] = v[tam-1-i];
     v[tam-1-i] = temp;
 
-    // Chama a funcao recursivamente para inverter o restante do vetor
-    // Usando o ponteiro para avancar no vetor e o tamanho decrementado
-    inverter(v, i+1, tam);
+    inverter2(v, i+1, tam);
 }
 
 int main(){
     int vetor[10] = {0,1,2,3,4,5,6,7,8,9};
+    int vetor2[10] = {0,1,2,3,4,5,6,7,8,9};
     
-    printf("Vetor antes da inversao: ");
+    printf("Vetor antes da inversao:\n");
     for(int i=0; i<10;i++){
         printf("%d ", vetor[i]);
     }
-    printf("\nVetor apos a inversao: ");
-    inverter(vetor, 0, 10);
+    
+    printf("\nVetor apos a inversao:\n");
+    inverter(vetor, 10);
     for(int i=0; i<10;i++){
         printf("%d ", vetor[i]);
+    }
+    
+    printf("\n");
+    inverter2(vetor2, 0, 10);
+    for(int i=0; i<10;i++){
+        printf("%d ", vetor2[i]);
     }
     return 0;
 }
